7ddfcd34adeb9d367de17cc4518f113f92b218bb
graceannserino <105411482+graceannserino@users.noreply.github.com>
2022-05-31 09:55:18 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QTableWidget
+from PyQt5.QtWidgets import QHeaderView
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import Qt
+
+import sys
+
+from methods import FundaMethods
+from variables import Variables
